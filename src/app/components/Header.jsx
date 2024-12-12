676634c72ace86c65ffd60324b9ed5ff9d6c267b
sarthik-7span <sarthik@7span.com>
2024-12-12 10:47:29 +0530

    footer

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import Logo from "../../../public/air-borne.png";
+import Logo from "../../assets/icons/air-borne.png";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div>hello
     </div>
   );
 }

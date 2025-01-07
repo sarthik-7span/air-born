ee3917919ec107aee328398b3d83b9d34bd036f6
patelsanam <sanam.patel@toptal.com>
2025-01-08 01:37:06 +0530

    xyz

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -129,7 +129,7 @@ const ContactForm = ({ inquiryFor }) => {
                   name="inquiry"
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-0 focus:border-primary block w-full p-2.5"
                   value={inquiryFor}
-                  disabled
+                  readOnly
                 />
               </div>
             )}
